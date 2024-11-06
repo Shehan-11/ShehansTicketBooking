41ff9938f48b7c2dd7de42518d99125d739d12ee
Shehan-11 <shehanathulgama@gmail.com>
2024-11-06 09:13:08 +0530

    Initial commit

---
+++ src/main/java/com/example/ShehansTicketBooking/ShehansTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShehansTicketBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShehansTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShehansTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShehansTicketBooking/ShehansTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShehansTicketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShehansTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
