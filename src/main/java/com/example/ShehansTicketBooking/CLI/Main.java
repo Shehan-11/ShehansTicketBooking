5fb8092dd54b218e1f2b37369010cbe1c403a6d6
Shehan Etulgama <121471956+Shehan-11@users.noreply.github.com>
2024-11-07 22:03:21 +0530

    DB Configuration

---
+++ src/main/java/com/example/ShehansTicketBooking/CLI/Main.java
@@ -0,0 +1,9 @@
+package com.example.ShehansTicketBooking.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting CLI Application...");
+        // Initialize CLI-specific configurations and services
+        // Run the CLI interface
+    }
+}
