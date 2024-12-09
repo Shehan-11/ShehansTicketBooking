8bad9adb173d5f32ab9a16432592a1b80ae4658e
Shehan Etulgama <121471956+Shehan-11@users.noreply.github.com>
2024-12-09 22:03:20 +0530

    Final

--- src/main/java/com/example/ShehansTicketBooking/Backend/Models/Configuration.java
+++ src/main/java/com/example/ShehansTicketBooking/Backend/Models/Configuration.java
@@ -33,44 +33,20 @@ public class Configuration {
         this.lastUpdateDate = lastUpdateDate;
     }
 
-    public Long getConfigID() {
-        return configID;
-    }
-
-    public void setConfigID(Long configID) {
-        this.configID = configID;
-    }
-
     public int getMaxTicketCapacity() {
         return maxTicketCapacity;
-    }
-
-    public void setMaxTicketCapacity(int maxTicketCapacity) {
-        this.maxTicketCapacity = maxTicketCapacity;
     }
 
     public int getTotalTickets() {
         return totalTickets;
     }
 
-    public void setTotalTickets(int totalTickets) {
-        this.totalTickets = totalTickets;
-    }
-
     public int getTicketReleaseRate() {
         return ticketReleaseRate;
     }
 
-    public void setTicketReleaseRate(int ticketReleaseRate) {
-        this.ticketReleaseRate = ticketReleaseRate;
-    }
-
     public int getCustomerRetrievalRate() {
         return customerRetrievalRate;
-    }
-
-    public void setCustomerRetrievalRate(int customerRetrievalRate) {
-        this.customerRetrievalRate = customerRetrievalRate;
     }
 
     public LocalDateTime getLastUpdateDate() {
